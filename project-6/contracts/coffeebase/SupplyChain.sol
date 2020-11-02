bd77aa7eba7e4eeb8735a673c6a4a435e537ca78
ben-stamm <40069838+bennystamm@users.noreply.github.com>
2020-11-02 14:08:03 -0500

    updates2

--- project-6/contracts/coffeebase/SupplyChain.sol
+++ project-6/contracts/coffeebase/SupplyChain.sol
@@ -161,7 +161,7 @@ contract SupplyChain is FarmerRole, DistributorRole, RetailerRole, ConsumerRole 
     // Add the new item as part of Harvest
         items[_upc].sku = sku;
         items[_upc].upc = _upc;
-        items[_upc].ownerID = owner;
+        items[_upc].ownerID = _originFarmerID;
         items[_upc].originFarmerID = _originFarmerID;
         items[_upc].originFarmName = _originFarmName;
         items[_upc].originFarmInformation = _originFarmInformation;
@@ -221,7 +221,7 @@ contract SupplyChain is FarmerRole, DistributorRole, RetailerRole, ConsumerRole 
     {
 
     // Update the appropriate fields - ownerID, distributorID, itemState
-        items[_upc].ownderID = msg.sender;
+        items[_upc].ownerID = msg.sender;
         items[_upc].distributorID = msg.sender;
         items[_upc].itemState = State.Sold;
     // Transfer money to farmer
