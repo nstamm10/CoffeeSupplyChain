231535812d4b15a58cc319751be19c12800a283d
ben-stamm <40069838+bennystamm@users.noreply.github.com>
2020-11-05 08:57:50 -0500

    download truffle hdwallet, add truffle-config.js

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -3,7 +3,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "https://rinkeby.infura.io/v3/89bc219a7fec48fcaaa9116059e19880@" // Match any network id
     }
   }
 };
